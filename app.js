a4020d8c572d622b77045bb694a65bc34aa22405
itohara22 <ito.k23018@gmail.com>
2023-04-27 17:47:21 +0530

    created

---
+++ app.js
@@ -0,0 +1,78 @@
+//stopping animation on resizing
+let resizeTimer;
+window.addEventListener("resize", () => {
+    document.body.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove("resize-animation-stopper");
+    }, 400);
+});
+
+
+// mobile navbar
+const primaryNav = document.querySelector('.nav_bar');
+const navToggle = document.querySelector('.nav_btn');
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+
+    if (visibility === "false") {
+        primaryNav.setAttribute('data-visible', true);
+    }
+    else if (visibility === "true") {
+        primaryNav.setAttribute('data-visible', false);
+    }
+})
+
+
+//on scroll animation
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        // console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+const form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    const status = document.getElementById("status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            status.innerHTML = "Your mail is sent";
+            status.classList.add('success')
+            form.reset()
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+                } else {
+                    status.innerHTML = "Oops! There was a problem"
+
+                }
+                status.classList.add('error')
+                form.reset()
+            })
+        }
+    }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+    });
+}
+form.addEventListener("submit", handleSubmit)
