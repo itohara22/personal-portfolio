1f82250eebd06671be01e82c435c5c27060d29de
itohara22 <ito.k23018@gmail.com>
2023-05-29 13:48:05 +0530

    nth of type

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 //stopping animation on resizing
 let resizeTimer;
 window.addEventListener("resize", () => {
-    document.body.classList.add("resize-animation-stopper");
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(() => {
-        document.body.classList.remove("resize-animation-stopper");
-    }, 400);
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
 });
 
 //hero mousemove effect
@@ -13,95 +13,95 @@ const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 let interval = null;
 
-document.querySelector("h1").onmouseover = event => {
-    let iteration = 0;
+document.querySelector("h1").onmouseover = (event) => {
+  let iteration = 0;
 
-    clearInterval(interval);
+  clearInterval(interval);
 
-    interval = setInterval(() => {
-        event.target.innerText = event.target.innerText
-            .split("")
-            .map((letter, index) => {
-                if (index < iteration) {
-                    return event.target.dataset.value[index];
-                }
-
-                return letters[Math.floor(Math.random() * 26)]
-            })
-            .join("");
-
-        if (iteration >= event.target.dataset.value.length) {
-            clearInterval(interval);
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return event.target.dataset.value[index];
         }
 
-        iteration += 1 / 3;
-    }, 30);
-}
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= event.target.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+};
 
 // mobile navbar
-const primaryNav = document.querySelector('.nav_bar');
-const navToggle = document.querySelector('.nav_btn');
+const primaryNav = document.querySelector(".nav_bar");
+const navToggle = document.querySelector(".nav_btn");
 
-navToggle.addEventListener('click', () => {
-    const visibility = primaryNav.getAttribute('data-visible');
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
 
-    if (visibility === "false") {
-        primaryNav.setAttribute('data-visible', true);
-    }
-    else if (visibility === "true") {
-        primaryNav.setAttribute('data-visible', false);
-    }
-})
-
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+  }
+});
 
 //on scroll animation
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        // console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show')
-        }
-    });
+  entries.forEach((entry) => {
+    // console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden')
+const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
-
-
+//form handling
 const form = document.getElementById("my-form");
 
 async function handleSubmit(event) {
-    event.preventDefault();
-    const status = document.getElementById("status");
-    var data = new FormData(event.target);
-    fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(response => {
-        if (response.ok) {
-            status.innerHTML = "Your mail is sent";
-            status.classList.add('success')
-            form.reset()
-        } else {
-            response.json().then(data => {
-                if (Object.hasOwn(data, 'errors')) {
-                    status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-                } else {
-                    status.innerHTML = "Oops! There was a problem"
-
-                }
-                status.classList.add('error')
-                form.reset()
-            })
-        }
-    }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form"
+  event.preventDefault();
+  const status = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = "Your mail is sent";
+        status.classList.add("success");
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.innerHTML = "Oops! There was a problem";
+          }
+          status.classList.add("error");
+          form.reset();
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! There was a problem submitting your form";
     });
 }
-form.addEventListener("submit", handleSubmit)
+form.addEventListener("submit", handleSubmit);
